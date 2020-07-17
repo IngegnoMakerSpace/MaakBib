89b8e3dea6416e7d1b16fef099c004129758dee6
Jari Valentine <jarivalentine@gmail.com>
2020-07-17 13:34:37 +0200

    Retro Game Maken MaakBib

--- _posts/2020-07-16-Retro-Game-Maken.md
+++ _posts/2020-07-16-Retro-Game-Maken.md
@@ -5,8 +5,8 @@ featured: false
 published: false
 authors: [guest]
 author_name: ""
-lowtech: true
-midtech: false
+lowtech: false
+midtech: true
 hightech: false
 DIT: true
 DIY: true
@@ -19,26 +19,60 @@ pijler_digitale_fabricatie : false
 pijler_physical_computing : true
 licence: CCBY-NC-SA
 categories: [physical computing]
-image: 
+image: assets/images/2020-07-16-Retro-Game-Maken/main.png
 tijd: 3u+
-print: 
+print: assets/images/2020-07-16-Retro-Game-Maken/Handleiding_RetroGaming_ArduinoNano.docx.pdf   
 downloads: 
-  - name: 'naam'
-    file: 
-materialen: [
-
-]
-tools: [
-    
-]
+  - name: 'Handleiding'
+    file: assets/images/2020-07-16-Retro-Game-Maken/Handleiding_RetroGaming_ArduinoNano.docx.pdf   
 ---
 
-Leer hoe je je eigen game programmeerd voor je eigen game console!
-
-*Deze handleiding werd ontwikkeld door **Naam** voor Maakbib.*
+Leer hoe je je eigen game programmeerd voor jou zelf gemaakte game console! Hier leer je werken met blokkencode en uiteindelijk hoe je een game maakt (Etch a Sketch).
 
 ## Benodigdheden
 
+<p style="margin: 0 0 0 0;"><strong>Materialen:</strong></p>
+<div class="benodigdheden">
+  <ul>
+    <li><a href="#">Retro Game Console!</a></li>
+ </ul>
+</div>
+
+![gameconsole]({{ site.baseurl }}/assets/images/2020-07-16-Retro-Game-Maken/main.jpg)
+
 {% include benodigdheden.html %}
 
-## Stap 1
+## Deel 1: Blokkencode
+Om te beginnen ga je naar <a href="http://blokkencode.ingegno.be" target="_blanc">blokkencode.ingegno.be</a>. Dit is een website waar we met blokken code kunnen schrijven voor de arduino. Als je blokkencode af is kan je de code bij ``Arduino Source Code`` copieëren en in je Arduino IDE zetten.
+
+**Hoe werken de blokken?**
+
+Je kan verschillende soorten blokken kiezen bij het menutje ``Blokken``. Hier vind je verschillende categorieën, bv. onder de categorie tijd zullen al de blokken staan die te maken hebben met tijd: 
+
+![gamemaken1]({{ site.baseurl }}/assets/images/2020-07-16-Retro-Game-Maken/gamemaken1.png)
+
+De blokken kan je dan allemaal aan een steken om een logische code te ontwikkelen. Je kan al de blokken vastnemen en naar de juiste plaats slepen. Als de blok in die plaats past gaat die vanzelf 'vastklikken'.
+
+![gamemaken2]({{ site.baseurl }}/assets/images/2020-07-16-Retro-Game-Maken/gamemaken2.png)
+
+**Hoe maak je een logische blokkencode?**
+
+Om een code te ontwikkelen begin je altijd met het declareren van je variabelen. Dat betekend dus dat je de Arduino gaat vertellen wat je allemaal gaat gebruiken en aan welke pin die componenten vast zitten.
+
+![gamemaken3]({{ site.baseurl }}/assets/images/2020-07-16-Retro-Game-Maken/gamemaken3.png)
+
+* Bij onze Retro Game Console zitten de pinnen zoals hierboven geconfigureerd.
+
+Als je een blok heb met een settings icoontje betekend dat dat je de blok kan aanpassen. Bij het 'Platform' blokje kan je dus kiezen wat voor pin je wilt toevoegen.
+
+![gamemaken4]({{ site.baseurl }}/assets/images/2020-07-16-Retro-Game-Maken/gamemaken4.png)
+
+* De Joystick X en Y pinnen zijn analoge pinnen, maar de knoppen zijn digitale pinnen.
+
+Nu dat je dat klaar hebt heb je de ``Arduino doe eerst: / Arduino herhaal voor altijd:`` blok nodig. In de ``Àrduino doe eerst`` scope ga je blokken zetten die maar 1 keer zullen uitgevoerd worden, in het begin. In de ``Arduino herhaal voor altijd`` scope ga je blokken zetten die blijven herhalen, als de laatste blok werd uitgevoerd begint de Arduino weer aan de eerst.
+
+![gamemaken5]({{ site.baseurl }}/assets/images/2020-07-16-Retro-Game-Maken/gamemaken5.png)
+
+* Als de Arduino opstart gaat het scherm voor 3 seconden vast op rood staan. Maar als je daarna op de knop drukt die op pin 1 is aangesloten zal het scherm wit worden voor 1 seconde en dan zwart worden.
+
+## Deel 2: Etch a Sketch maken
--- assets/downloads/2020-07-13-Mastersword/Mastersword/Mastersword.ino
+++ assets/downloads/2020-07-13-Mastersword/Mastersword/Mastersword.ino
@@ -2,33 +2,56 @@
 #include <Adafruit_ST7735.h> // version 1.2.7
 #include <SPI.h>
 
-boolean Drukken;
-int X_waarde;
-int Y_waarde;
-int horizontale_richting;
-int verticale_richting;
+int kleurdot;
+int achtergrondkleur;
 int Xpos;
 int Ypos;
+int X_richting;
+int Y_richting;
 boolean wandel;
 int Xpos_oud;
 int Ypos_oud;
-int Joystick_druk = 0;
+int Joystick_druk = 4;
+int knopA = 2;
+int knopB = 3;
 int Joystick_X = A0;
-int Joystick_Y = A0;
+int Joystick_Y = A1;
 
 boolean Joystick_druk_PRESSED = LOW;
 
+boolean knopA_PRESSED = LOW;
 
-// Connect TFT1_7 to Arduino Nano - Vcc & GND, LED to 3.3V,
+boolean knopB_PRESSED = LOW;
+
+
+// Connect TFT1 to Arduino Nano - Vcc & GND, LED to 3.3V,
 // SDA pin to NANO pin 11 (MOSI)
 // SDO pin to NANO pin 12 (MISO)
 // SCK pin to NANO pin 13 (SCK)
-#define TFT1_7_CS     10 // CS pin to NANO pin 10
-#define TFT1_7_RST    8  // RST (RESET) pin to NANO pin 8
-#define TFT1_7_DC     9  // AO or D/C pin to NANO pin 9
+#define TFT1_CS     10 // CS pin to NANO pin 10
+#define TFT1_RST    8  // RST (RESET) pin to NANO pin 8
+#define TFT1_DC     9  // AO or D/C pin to NANO pin 9
 
-Adafruit_ST7735 TFT1_7 = Adafruit_ST7735(TFT1_7_CS,  TFT1_7_DC, TFT1_7_RST);
+Adafruit_ST7735 TFT1 = Adafruit_ST7735(TFT1_CS,  TFT1_DC, TFT1_RST);
 
+long knopAbuttonTimer = 0;
+#define knopAminShortPressTime 80
+#define knopAlongPressTime 750
+boolean knopAbuttonActive = false;
+boolean knopAlongPressActive = false;
+#define knopANOPRESS    0
+#define knopASHORTPRESS 1
+#define knopALONGPRESS  2
+int knopAPressType = knopANOPRESS;
+long knopBbuttonTimer = 0;
+#define knopBminShortPressTime 80
+#define knopBlongPressTime 750
+boolean knopBbuttonActive = false;
+boolean knopBlongPressActive = false;
+#define knopBNOPRESS    0
+#define knopBSHORTPRESS 1
+#define knopBLONGPRESS  2
+int knopBPressType = knopBNOPRESS;
 
 void TFT1drawtext(String text, uint16_t color, int size, int x, int y) {
   TFT1.setCursor(x, y);
@@ -38,22 +61,22 @@ void TFT1drawtext(String text, uint16_t color, int size, int x, int y) {
   TFT1.println(text);
 }
 
-static unsigned int Vlinder8x8[] =
-{ TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0x00, 0x00, 0x00), TFT1.color565(0x00, 0x00, 0x00), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0xff, 0xff, 0x00),
-TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00),
-TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00),
-TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0x66, 0x66, 0x66),
-TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0x66, 0x66, 0x66),
-TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00),
-TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00),
-TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0xff, 0xff, 0x00)
+static unsigned int Sketch8x8[] =
+{ TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x66), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0xff, 0xff, 0x33), TFT1.color565(0xff, 0xff, 0x66), TFT1.color565(0xff, 0xff, 0x33), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0xff, 0xff, 0x00),
+TFT1.color565(0xff, 0xff, 0x66), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0x66, 0x66, 0x66),
+TFT1.color565(0xff, 0xff, 0x33), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0x66, 0x66, 0x66),
+TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0xff, 0xff, 0x66),
+TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0xff, 0xff, 0x00), TFT1.color565(0xff, 0xff, 0x66),
+TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0x66, 0x66, 0x66),
+TFT1.color565(0xff, 0xff, 0x66), TFT1.color565(0xff, 0xff, 0x66), TFT1.color565(0xff, 0x00, 0x00), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0x33, 0x33, 0xff), TFT1.color565(0x33, 0x33, 0xff), TFT1.color565(0xcc, 0x33, 0xcc), TFT1.color565(0x66, 0x66, 0x66),
+TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0xff, 0x00, 0x00), TFT1.color565(0x33, 0xff, 0x33), TFT1.color565(0x33, 0xff, 0x33), TFT1.color565(0x66, 0x66, 0x66), TFT1.color565(0xcc, 0x33, 0xcc), TFT1.color565(0x33, 0xff, 0xff)
 };
 
-#define VlinderW            8     // sprite width
-#define VlinderH            8     // mario height
-#define VlinderW2           4     // half width
-#define VlinderH2           4     // half height
-unsigned char Vlinderpx, Vlinderpy;
+#define SketchW            8     // sprite width
+#define SketchH            8     // mario height
+#define SketchW2           4     // half width
+#define SketchH2           4     // half height
+unsigned char Sketchpx, Sketchpy;
 
 
 // ----------------------
@@ -64,109 +87,264 @@ static short TFT1tmpx, TFT1tmpy, TFT1tmps1, TFT1tmps2;
 
 
 
+int ard_effect0_status = -1;
+unsigned long ard_effect0_start, ard_effect0_time;
+#define EFFECT0_PERIOD 50
+
 // Deze functie beschrijven...
-void lees_joystick() {
-  Drukken = (digitalRead(Joystick_druk) == Joystick_druk_PRESSED);
-  X_waarde = analogRead(Joystick_X);
-  Y_waarde = analogRead(Joystick_Y);
-  horizontale_richting = 0;
-  if (X_waarde < 480) {
-    horizontale_richting = 1;
-  } else if (X_waarde > 520) {
-    horizontale_richting = -1;
+void doe_een_stap() {
+  //Variables of this effect are reffered to with ard_effect0
+  boolean restart = false;
+  ard_effect0_time = millis() - ard_effect0_start;
+  if (ard_effect0_time > EFFECT0_PERIOD) {
+    //end effect, make sure it restarts
+    if (ard_effect0_status > -1) {
+    }
+    restart = true;
+    ard_effect0_status = -1;
+    ard_effect0_start = ard_effect0_start + ard_effect0_time;
+    ard_effect0_time = 0;
   }
-  verticale_richting = 0;
-  if (Y_waarde < 480) {
-    verticale_richting = -1;
-  } else if (Y_waarde > 520) {
-    verticale_richting = 1;
+  if (not restart && ard_effect0_status == -1) {
+    ard_effect0_status = 0;
+    ard_effect0_start = ard_effect0_start + ard_effect0_time;
+    ard_effect0_time = 0;
+  lees_joystick_wandel();
   }
 }
 
 
-void setup() {
-  pinMode(Joystick_druk, INPUT_PULLUP);
-  pinMode(Joystick_X, INPUT);
-  pinMode(Joystick_Y, INPUT);
-  TFT1_7.initR(INITR_BLACKTAB);
 
-  TFT1.fillScreen(TFT1.color565(0x66, 0x66, 0x66) );
-  TFT1drawtext("Retro", TFT1.color565(0xff, 0x00, 0x00), 3, 30, 5);
-  delay(500);
-  TFT1drawtext("Games", TFT1.color565(0xff, 0x00, 0x00), 3, 30, 60);
-  while (!(digitalRead(Joystick_druk) == Joystick_druk_PRESSED)) {
+void handleknopAPress() {
+  knopAPressType = knopANOPRESS;
+      if (digitalRead(knopA) == knopA_PRESSED) {
+        if (knopAbuttonActive == false) {
+          knopAbuttonActive = true;
+          knopAbuttonTimer = millis();
+        }
+        if ((millis() - knopAbuttonTimer > knopAlongPressTime) && (knopAlongPressActive == false)) {
+          knopAlongPressActive = true;
+          knopAPressType = knopALONGPRESS;
+        }
+      } else {
+        if (knopAbuttonActive == true) {
+          if (knopAlongPressActive == true) {
+            knopAlongPressActive = false;
+          } else {
+            //avoid fast fluctuations to be identified as a click
+            if (millis() - knopAbuttonTimer > knopAminShortPressTime)
+              knopAPressType = knopASHORTPRESS;
+          }
+          knopAbuttonActive = false;
+        }
+      }
+}
+
+
+
+void handleknopBPress() {
+  knopBPressType = knopBNOPRESS;
+      if (digitalRead(knopB) == knopB_PRESSED) {
+        if (knopBbuttonActive == false) {
+          knopBbuttonActive = true;
+          knopBbuttonTimer = millis();
+        }
+        if ((millis() - knopBbuttonTimer > knopBlongPressTime) && (knopBlongPressActive == false)) {
+          knopBlongPressActive = true;
+          knopBPressType = knopBLONGPRESS;
+        }
+      } else {
+        if (knopBbuttonActive == true) {
+          if (knopBlongPressActive == true) {
+            knopBlongPressActive = false;
+          } else {
+            //avoid fast fluctuations to be identified as a click
+            if (millis() - knopBbuttonTimer > knopBminShortPressTime)
+              knopBPressType = knopBSHORTPRESS;
+          }
+          knopBbuttonActive = false;
+        }
+      }
+}
+
+
+// Deze functie beschrijven...
+void start_dot() {
+  /* "Scherm is 128 x 160" */
+  Xpos = 64 - 1;
+  Ypos = 80 - 1;
+  kleurdot = 0;
+  TFT1.fillRect(Xpos, Ypos, 2, 2, TFT1.color565(0xff, 0x66, 0x00));
+}
+
+// Deze functie beschrijven...
+void wijzig_achtergrond() {
+  if (achtergrondkleur == 0) {
+    TFT1.fillScreen(TFT1.color565(0x66, 0x66, 0x66) );
+  } else if (achtergrondkleur == 1) {
+    TFT1.fillScreen(TFT1.color565(0xff, 0xff, 0xff) );
+  } else if (achtergrondkleur == 2) {
+    TFT1.fillScreen(TFT1.color565(0x00, 0x00, 0x00) );
+  } else if (achtergrondkleur == 3) {
+    TFT1.fillScreen(TFT1.color565(0xff, 0x00, 0x00) );
+  } else if (achtergrondkleur == 4) {
+    TFT1.fillScreen(TFT1.color565(0xff, 0xcc, 0x66) );
+  } else if (achtergrondkleur == 5) {
+    TFT1.fillScreen(TFT1.color565(0x33, 0xff, 0x33) );
+  } else if (achtergrondkleur == 6) {
+    TFT1.fillScreen(TFT1.color565(0x66, 0xcc, 0xcc) );
+  } else if (achtergrondkleur == 7) {
+    TFT1.fillScreen(TFT1.color565(0x33, 0x33, 0xff) );
+  } else if (achtergrondkleur == 8) {
+    TFT1.fillScreen(TFT1.color565(0xcc, 0x33, 0xcc) );
+  } else {
+    TFT1.fillScreen(TFT1.color565(0x99, 0x99, 0x00) );
   }
+}
+
+// Deze functie beschrijven...
+void lees_joystick_wandel() {
+  X_richting = analogRead(Joystick_X);
+  Y_richting = analogRead(Joystick_Y);
+  wandel = false;
+  Xpos_oud = Xpos;
+  Ypos_oud = Ypos;
+  if (X_richting > 700) {
+    wandel = true;
+    Xpos = Xpos - 1;
+  } else if (X_richting < 300) {
+    wandel = true;
+    Xpos = Xpos + 1;
+  }
+  if (Y_richting > 700) {
+    wandel = true;
+    Ypos = Ypos + 1;
+  } else if (Y_richting < 300) {
+    wandel = true;
+    Ypos = Ypos - 1;
+  }
+  if (wandel) {
+    /* "we laten de oude dot gewoon staan" */
+  }
+  /* "We corrigeren zodat we scherm niet verlaten!" */
+  /* "Scherm is 128 x 160" */
+  if (Xpos < 0) {
+    Xpos = 0;
+  } else if (Xpos > 126) {
+    Xpos = 126;
+  }
+  if (Ypos < 0) {
+    Ypos = 0;
+  } else if (Ypos > 158) {
+    Ypos = 158;
+  }
+  if (kleurdot == 0) {
+    TFT1.fillRect(Xpos, Ypos, 2, 2, TFT1.color565(0xff, 0x66, 0x00));
+  } else if (kleurdot == 1) {
+    TFT1.fillRect(Xpos, Ypos, 2, 2, TFT1.color565(0xff, 0xff, 0xff));
+  } else if (kleurdot == 2) {
+    TFT1.fillRect(Xpos, Ypos, 2, 2, TFT1.color565(0x00, 0x00, 0x00));
+  } else if (kleurdot == 3) {
+    TFT1.fillRect(Xpos, Ypos, 2, 2, TFT1.color565(0xff, 0x00, 0x00));
+  } else if (kleurdot == 4) {
+    TFT1.fillRect(Xpos, Ypos, 2, 2, TFT1.color565(0xff, 0xcc, 0x66));
+  } else if (kleurdot == 5) {
+    TFT1.fillRect(Xpos, Ypos, 2, 2, TFT1.color565(0x33, 0xff, 0x33));
+  } else if (kleurdot == 6) {
+    TFT1.fillRect(Xpos, Ypos, 2, 2, TFT1.color565(0x66, 0xcc, 0xcc));
+  } else if (kleurdot == 7) {
+    TFT1.fillRect(Xpos, Ypos, 2, 2, TFT1.color565(0x33, 0x33, 0xff));
+  } else if (kleurdot == 8) {
+    TFT1.fillRect(Xpos, Ypos, 2, 2, TFT1.color565(0xcc, 0x33, 0xcc));
+  } else {
+    TFT1.fillRect(Xpos, Ypos, 2, 2, TFT1.color565(0x99, 0x99, 0x00));
+  }
+}
+
+// Deze functie beschrijven...
+void startscherm() {
   TFT1.fillScreen(TFT1.color565(0x66, 0x66, 0x66) );
-  Xpos = 60;
-  Ypos = 60;
+  TFT1drawtext("Retro", TFT1.color565(0xff, 0xff, 0x66), 3, 30, 5);
+  delay(2000);
+  TFT1drawtext("Games", TFT1.color565(0x66, 0xff, 0x99), 3, 30, 60);
 
   // draw sprite
   // ---------------
-  TFT1tmpx = VlinderW - 1; //width sprite
+  TFT1tmpx = SketchW - 1; //width sprite
   do {
-    Vlinderpx = Xpos + 2 * TFT1tmpx;
-    // draw Vlinder at new position
-    TFT1tmpy = VlinderH - 1;
+    Sketchpx = 60 + 3 * TFT1tmpx;
+    // draw Sketch at new position
+    TFT1tmpy = SketchH - 1;
     do {
-      Vlinderpy = Ypos + 2 * TFT1tmpy ;
-      TFT1tmps1 = 2 - 1; //scale
+      Sketchpy = 100 + 3 * TFT1tmpy ;
+      TFT1tmps1 = 3 - 1; //scale
       do {
-        TFT1tmps2 = 2 - 1; //scale
+        TFT1tmps2 = 3 - 1; //scale
         do {
-          TFT1.drawPixel(Vlinderpx + TFT1tmps1, Vlinderpy + TFT1tmps2, Vlinder8x8[TFT1tmpx + (TFT1tmpy * VlinderW)]);
+          TFT1.drawPixel(Sketchpx + TFT1tmps1, Sketchpy + TFT1tmps2, Sketch8x8[TFT1tmpx + (TFT1tmpy * SketchW)]);
           } while (TFT1tmps2--);
       } while (TFT1tmps1--);
     } while (TFT1tmpy--);
   } while (TFT1tmpx--);
 
-  delay(3000);
+  while ((digitalRead(knopA) == knopA_PRESSED)) {
+  }
+  TFT1.fillScreen(TFT1.color565(0x66, 0x66, 0x66) );
+}
+
+
+void setup() {
+  pinMode(Joystick_druk, INPUT_PULLUP);
+  pinMode(knopA, INPUT);
+  pinMode(knopB, INPUT);
+  pinMode(Joystick_X, INPUT);
+  pinMode(Joystick_Y, INPUT);
+  TFT1.initR(INITR_BLACKTAB);
+  ard_effect0_status = -1;
+  ard_effect0_start = millis();
+
+
+  startscherm();
+  start_dot();
 
 }
 
 void loop() {
-  lees_joystick();
-  wandel = false;
-  Xpos_oud = Xpos;
-  Ypos_oud = Ypos;
-  Xpos = Xpos + horizontale_richting;
-  Ypos = Ypos + verticale_richting;
-  if (Xpos != Xpos_oud || Ypos != Ypos_oud) {
-    wandel = true;
-    TFT1.drawRect(Xpos_oud, Ypos_oud, 16, 16, TFT1.color565(0x66, 0x66, 0x66));
-    /* "Corrigeren om scherm niet te verlaten" */
-    /* "scherm is 128x160" */
-    if (Xpos < 0) {
-      Xpos = 0;
-    } else if (Xpos > 112) {
-      Xpos = 112;
-    }
-    if (Ypos < 0) {
-      Ypos = 0;
-    } else if (Ypos > 144) {
-      Ypos = 144;
-    }
+  doe_een_stap();
+  handleknopAPress();
 
-    // draw sprite
-    // ---------------
-    TFT1tmpx = VlinderW - 1; //width sprite
-    do {
-      Vlinderpx = Xpos + 2 * TFT1tmpx;
-      // draw Vlinder at new position
-      TFT1tmpy = VlinderH - 1;
-      do {
-        Vlinderpy = Ypos + 2 * TFT1tmpy ;
-        TFT1tmps1 = 2 - 1; //scale
-        do {
-          TFT1tmps2 = 2 - 1; //scale
-          do {
-            TFT1.drawPixel(Vlinderpx + TFT1tmps1, Vlinderpy + TFT1tmps2, Vlinder8x8[TFT1tmpx + (TFT1tmpy * VlinderW)]);
-            } while (TFT1tmps2--);
-        } while (TFT1tmps1--);
-      } while (TFT1tmpy--);
-    } while (TFT1tmpx--);
-
-    /* "10 stappen per sec max" */
-    delay(100);
+  if (knopAPressType == knopASHORTPRESS) {
+    //START STATEMENTS SHORT PRESS
+    kleurdot = kleurdot + 1;
+    if (kleurdot > 9) {
+      kleurdot = 0;
+    }
+    //END  STATEMENTS SHORT PRESS
+  } else if (knopAPressType == knopALONGPRESS) {
+    //START STATEMENTS LONG PRESS
+    //END  STATEMENTS LONG PRESS
+  } else if (!knopAlongPressActive && digitalRead(knopA) == knopA_PRESSED) {
+    //START STATEMENTS PRESS
+    //END  STATEMENTS PRESS
   }
+
+  handleknopBPress();
+
+  if (knopBPressType == knopBSHORTPRESS) {
+    //START STATEMENTS SHORT PRESS
+    achtergrondkleur = achtergrondkleur + 1;
+    if (achtergrondkleur > 9) {
+      achtergrondkleur = 0;
+    }
+    wijzig_achtergrond();
+    //END  STATEMENTS SHORT PRESS
+  } else if (knopBPressType == knopBLONGPRESS) {
+    //START STATEMENTS LONG PRESS
+    //END  STATEMENTS LONG PRESS
+  } else if (!knopBlongPressActive && digitalRead(knopB) == knopB_PRESSED) {
+    //START STATEMENTS PRESS
+    //END  STATEMENTS PRESS
+  }
+
 
 }
